55d02990cd9093b4ce34d2eedb55c1c0a53c1b19
Basava Raju D <basava@mac.lan>
2018-09-01 17:18:34 +0530

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#ifndef MAIN_CPP_INCLUDED
+#define MAIN_CPP_INCLUDED
+#include<stdio.h>
+#include "sample.h"
+
+int main(void)
+{
+
+    printf("Hello World");
+    print();
+
+    return 0;
+
+
+}
+
+
+
+
+
+#endif // MAIN_CPP_INCLUDED
---
+++ sample.cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int print(void){
+
+ printf("\nHello World from SAMPLE.CPP ");
+
+}
